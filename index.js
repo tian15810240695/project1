211f43d6a8764d58f35dea404a2c0d328539df1c
ZHANGYIM <1094160929@qq.com>
2018-06-14 10:24:05 +0800

    ZM先生修改了田贝贝的index.js文件

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 var oLi=document.getElementsByTagName('li');
+for(var i =0; i<oLi.length;i++){
+    oLi[i].index = i;
+    oLi[i].onclick=function(){
+        for(var j =0;oLi.length;j++){
+            oLi[j].className="active";
+        }
+        
+    }
+}
