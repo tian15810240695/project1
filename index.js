2ae07263af30620e0ebcff5e5a96c50c905be879
465634186@qq.com <465634186@qq.com>
2018-06-15 14:21:40 +0800

    完整的选项卡

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
+var oP=document.getElementById('options');
+var oL=document.getElementById('list');
 var oLi=document.getElementsByTagName('li');
-var oBox=document.getElementsByClassName('box');
+var oBox=document.getElementsByTagName('li');
 for(var i =0; i<oLi.length;i++){
     oLi[i].index = i;
     oLi[i].onclick=function(){
         for(var j =0;oLi.length;j++){
             oLi[j].className="";
+            oBox[j].style.display="none";
         }
         oLi[this.index].className="active";
-        for(var i=0;i<oBox.length;i++){
-            
-        }
-        
-        
+        oBox[this.index].style.display="block";
     }
 }
