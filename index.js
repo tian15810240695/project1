364443627bc869eeb0c7c8881069c0013fc419cd
465634186@qq.com <465634186@qq.com>
2018-06-15 08:40:45 +0800

    在分支2上修改了index.js文件

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var oLi=document.getElementsByTagName('li');
+var oBox=document.getElementsByClassName('box');
 for(var i =0; i<oLi.length;i++){
     oLi[i].index = i;
     oLi[i].onclick=function(){
         for(var j =0;oLi.length;j++){
-            oLi[j].className="active";
+            oLi[j].className="";
         }
+        oLi[this.index].className="active";
+        for(var i=0;i<oBox.length;i++){
+            oBox[i].style.display="none";
+        }
+        oBox[this.index].style.display="block";
         
     }
 }
